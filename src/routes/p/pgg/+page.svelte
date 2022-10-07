9126245f91a12245852bb4f08b592f5b32efe77d
Edward Smale <essmale2005@gmail.com>
2022-10-07 15:19:40 +0100

    Update PGG description

--- src/routes/p/pgg/+page.svelte
+++ src/routes/p/pgg/+page.svelte
@@ -11,23 +11,14 @@
         especially with a decent sized team.
         <br><br>
         One of my best examples of working into an existing codebase, I was tasked with maintaining the biggest game mode
-        on the platform 
+        on the platform, implementing user requests and working with artist.
+        <br><br>
+        Unfortunately the project was cut short due to multiple inevitable failures, as if it collapsed under its own weight.
+        Mouthwash.GG was my successor project, written below.
     </p>
     
     <h2 class="text-4xl">Mouthwash.GG</h2>
     <p>
-        Talking about Edith wouldn't be complete without talking about Chronicle, an app I am making along-side Edith, taking control
-        of conversations between characters, interactions with objects, character portraits, timeline/screenplay and save data. It's
-        entirely friendly for non-developers, making it a good tool for development on Edith; working with an artist who otherwise
-        would have no other way to write dialogues and interactions directly into the game.
-        <br><br>
-        I developed integration with <a href="/t/unity">Unity</a> so that the Chronicle files are recognised as ScriptableObjects and assets,
-        meaning they can be referenced directly and bundled into the game.
-        <br><br>
-        Chronicle was a great experience to learn about user interfaces, file-system handling and complex web-based controls so that it
-        feels like a native app. It's written in <a href="/l/js">TypeScript</a> and <a href="/t/svelte">Svelte</a>, and built ontop of 
-        <a href="https://tauri.app">Tauri</a>. I also used <a href="https://tailwindcss.com">TailwindCSS</a> for the first time.
-        <br><br>
-        <img alt="a screenshot showing the Chronicle dashboard" src="/chronicle-screenshot.png">
+        
     </p>
 </div>
