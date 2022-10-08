26dca1dd6baa5ff1e3230748056cf421e9c948f9
Edward Smale <essmale2005@gmail.com>
2022-10-08 18:40:38 +0100

    Fix link hrefs

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -23,8 +23,8 @@
                 <span class="text-2xl">Full-stack developer</span>
             </div>
             <div class="flex flex-col">
-                <a href="text-slate-800 https://github.com/edqx">GITHUB</a>
-                <a href="text-slate-800 mailto:edward@midlight.studio">CONTACT</a>
+                <a class="text-slate-800" href="https://github.com/edqx">GITHUB</a>
+                <a class="text-slate-800" href="mailto:edward@midlight.studio">CONTACT</a>
             </div>
         </div>
         <div class="w-full h-32 mt-auto arrows">
